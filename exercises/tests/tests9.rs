d422b88b1f5cdd9351a34ac4786773e33542336b
Arrendar <U0phx20001@outlook.com>
2023-10-22 20:14:20 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,16 +27,19 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-#[no_mangle]
-#[link_name = "my_demo_function"]
-pub fn my_demo_function(a: u32) -> u32 {
-    a
+
+extern "Rust" {
+    fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
+    fn my_demo_function_alias(a: u32) -> u32;
 }
 
-#[no_mangle]
-#[link_name = "my_demo_function_alias"]
-pub fn my_demo_function_alias(a: u32) -> u32 {
-    my_demo_function(a)
+mod Foo {
+    // No `extern` equals `extern "Rust"`.
+    #[no_mangle]
+    fn my_demo_function(a: u32) -> u32 {
+        a
+    }
 }
 
 #[cfg(test)]
@@ -45,6 +48,12 @@ mod tests {
 
     #[test]
     fn test_success() {
+        // The externally imported functions are UNSAFE by default
+        // because of untrusted source of other languages. You may
+        // wrap them in safe Rust APIs to ease the burden of callers.
+        //
+        // SAFETY: We know those functions are aliases of a safe
+        // Rust function.
         unsafe {
             my_demo_function(123);
             my_demo_function_alias(456);
